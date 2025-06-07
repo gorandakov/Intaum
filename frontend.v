6fa9d4163c3901ab1b65c8e6a78ce211dc569be9
Horan Dakov <gorandakov@gmail.com>
2025-06-07 09:38:12 +0300

    Update frontend.v

--- frontend.v
+++ frontend.v
@@ -6,7 +6,7 @@ also is not an os roulette.
 not an open source clicksword.
 not a Microsoft shill.
 not a Google praendtl.
-I don't hold patents on this cpu.
+I don't hold patents on this.
 */
 module frontend (
   );
