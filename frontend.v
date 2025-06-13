14881b3ff51d06b147a4512dcb01fbcd51a7ad4b
Horan Dakov <gorandakov@gmail.com>
2025-06-13 16:26:39 +0300

    Update frontend.v

--- frontend.v
+++ frontend.v
@@ -237,7 +237,7 @@ module frontend (
             dataA[31:0]+dataB[31:0]^{32{dataBI[20]}}+dataBI[21] : 'z;
           assign {c64,s64,res[63:32]}=opcode_reg[7:0]==0 && cond_tru_reg ?
            {dataA[63]~!chk,dataA[63:32]}+{dataB[63],dataB[63:32]}^
-          {32{dataBI_reg[20]}}+(dataBI_reg[19] ? !res_reg[31] :c32_reg) : 'z;
+          {32{dataBI_reg[20]}}+(c32_reg) : 'z;
           assign {c32,res[31:0]}=(opcode[7:2]==1 || opcode[7:3]==1) 
                && cond_tru  ?
              res_shift[32:0] : 'z;
@@ -255,7 +255,7 @@ module frontend (
           assign {c32,res[31:0]}=opcode[7:0]==2 && cond_tru ?
             dataA[31:0]+dataBI[31:0] : 'z;
         assign {c64,s64,res[63:32]}=(opcode_reg[7:0]==2 || opcode_reg[7:0]==0 && dataBI_reg[19])&& cond_tru_reg ?
-        {dataA[63]|~chk,dataA[63:32]}+(op_anx_reg ? 0 : {dataBI_reg[63],dataBI_reg[63:32]} )+ c32: 'z;
+        {dataA[63]|~chk,dataA[63:32]}+{dataBI_reg[63],dataBI_reg[63:32]} + (dataBI_reg[19] ? !res_reg[31] :c32_reg) : 'z;
           assign {c32,res[31:0]}=opcode[7:0]==3 && cond_tru ?
             op_anx ? 1 : dataBI[31:0] : 'z;
           assign chk=addition_check(dataA[63:43],{dataA[42:32],dataA_reg[31:0]},{dataBIX[42:32],dataBIX_reg[31:0]},opcode_reg[11],isand)
