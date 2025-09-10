a8d2c8234844e1ce6bf425d5364f6bdc5f9ca74c
Goran Dakov <gorandakov@gmail.com>
2025-09-10 16:52:48 +0100

    @@

--- fpuprod64.v
+++ fpuprod64.v
@@ -24,6 +24,8 @@ module fpuprod64(
   reg [63:63] A_reg2;
   reg [63:63] B_reg;
   reg [63:63] B_reg2;
+  reg pookg_reg;
+  reg pookg_reg2;
 
   always @(posedge clk) begin
     prodA_reg<=prodA;
@@ -38,6 +40,8 @@ module fpuprod64(
     A_reg2<=A_reg;
     B_reg<=B[63];
     B_reg2<=B_reg;
+    pookg_reg<=pookg;
+    pookg_reg2<=pookg_reg;
   end
 
 
@@ -45,12 +49,12 @@ assign prodA={1'b1,A[52:0]}*{1'b1,B[52:0]}+{107'b0,rnd}<<53;
 
 assign prodB={1'b1,A[52:0]}*{1'b1,B[52:0]}+{107'b0,rnd}<<52;
 
-assign res[52:0]=prodA_reg2[107] | prodB_reg2[107] && ~pookg ? prodA_reg2[106:54] : 'z;
-assign res[52:0]=~(prodA_reg2[107] | prodB_reg2[107]) && ~pookg ? prodB_reg2[105:53] : 'z;
-assign res[52:0]=prodA_reg2[107] | prodB_reg2[107] && pookg ? prodA_reg2[53:1] : 'z;
-assign res[52:0]=~(prodA_reg2[107] | prodB_reg2[107]) && pookg ? prodB_reg2[52:0] : 'z;
+assign res[52:0]=prodA_reg2[107] | prodB_reg2[107] && ~pookg_reg2 ? prodA_reg2[106:54] : 'z;
+assign res[52:0]=~(prodA_reg2[107] | prodB_reg2[107]) && ~pookg_reg2 ? prodB_reg2[105:53] : 'z;
+assign res[52:0]=prodA_reg2[107] | prodB_reg2[107] && pookg_reg2 ? prodA_reg2[53:1] : 'z;
+assign res[52:0]=~(prodA_reg2[107] | prodB_reg2[107]) && pookg_reg2 ? prodB_reg2[52:0] : 'z;
 assign {c,ae}=A[62:53]+B[62:53]-bias;
-assign res[62:53]=pookg ? (ae_reg2-10'd53)|{10{c_reg2}} : ae_reg2|{10{c_reg2}};
+assign res[62:53]=pookg_reg2 ? (ae_reg2-10'd53)|{10{c_reg2}} : ae_reg2|{10{c_reg2}};
 assign res[63]=A_reg2[63]^B_reg2[63];
 
 endmodule
